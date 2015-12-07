553fdef384057857683be92bce59a67ec04365c5
harrisc2 <harrisc2@umbc.edu>
2015-12-07 13:07:46 -0500

    created footer file

---
+++ footer.php
@@ -0,0 +1,15 @@
+<html>
+<footer>
+<ul>
+<li><a href="02StudHome.php">Home</a></li>
+
+<li><a href="http://coeit.umbc.edu">About COEIT</a></li>
+
+<li><a href="http://advising.coeit.umbc.edu/contact-us/">Contact Us</a></li>
+
+<li><a href="">Equal Opportunity</a></li>	
+
+</ul>
+<h3 class = "location">© 2010 University of Maryland, Baltimore County. • 1000 Hilltop Circle, Baltimore, MD 21250 • 410-455-1000</h3>
+</footer>
+</html>
