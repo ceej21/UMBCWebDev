8de3dc8603d29f6fa04792ecdbb13b451d0d5975
harrisc2 <harrisc2@umbc.edu>
2015-12-07 19:25:22 -0500

    updated footer- was missing link in EqO

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 
 <li><a href="http://advising.coeit.umbc.edu/contact-us/">Contact Us</a></li>
 
-<li><a href="">Equal Opportunity</a></li>	
+<li><a href="http://humanrelations.umbc.edu/non-discrimination/ ">Equal Opportunity</a></li>	
 
 </ul>
 <h3 class = "location">© 2010 University of Maryland, Baltimore County. • 1000 Hilltop Circle, Baltimore, MD 21250 • 410-455-1000</h3>
